539df2ed82752af92ed8912392b75ef11dc26cc5
Kush Bajaria <bajariakush@gmail.com>
2025-08-08 10:37:18 -0700

    cleaning up file structure/deleting unnecessary files

--- workscheduler/classes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import User, db
-from .schedule import Schedule
-
-__all__ = ['User', 'Schedule', 'db']
--- workscheduler/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .decorators import login_required, admin_required
-from .utils import generate_shifts, get_week_dates, datetimeformat
-
-__all__ = ['login_required', 'admin_required', 'generate_shifts', 'get_week_dates', 'datetimeformat']
